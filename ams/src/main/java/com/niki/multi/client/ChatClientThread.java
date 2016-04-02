4dc405f3a672c5b18e4fb7d0f4ab72a5e576a72b
mukthar-am <mukthar.am@gmail.com>
2016-04-02 21:26:10 +0530

    final commit with multi chat window JFrame threads

--- ams/src/main/java/com/niki/multi/client/ChatClientThread.java
+++ ams/src/main/java/com/niki/multi/client/ChatClientThread.java
@@ -1,22 +1,30 @@
 package com.niki.multi.client;
 
+import com.sun.javafx.tk.*;
+import com.sun.javafx.tk.Toolkit;
+
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.concurrent.*;
 
 /**
  * Created by mukthar.ahmed on 4/2/16.
  */
 public class ChatClientThread extends Thread {
+
     private String user = "";
     private BufferedReader in;
     private PrintWriter out;
     private JFrame frame = null;
     private JTextField textField = new JTextField(40);
     private JTextArea messageArea = new JTextArea(8, 40);
+
+    private boolean ThreadRunFlag = true;
 
     public ChatClientThread(String name) {
         super(name);
@@ -45,51 +53,57 @@ public class ChatClientThread extends Thread {
             }
         });
 
-
-
     }
 
     public void run() {
-        System.out.println("+ Started the client thread...");
-        String serverAddress = "localhost";
+        while (ThreadRunFlag) {
+            System.out.println("+ Started the client thread...");
+            String serverAddress = "localhost";
 
-        // Process all messages from server, according to the protocol.
-        String srvResp = null;
-        try {
-            Socket socket = new Socket(serverAddress, 6061);
-            out = new PrintWriter(socket.getOutputStream(), true);
+            // Process all messages from server, according to the protocol.
+            String srvResp = null;
+            try {
+                Socket socket = new Socket(serverAddress, 6061);
+                out = new PrintWriter(socket.getOutputStream(), true);
 
-            DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+                DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
 
-            while (true) {
-                srvResp = dataInputStream.readUTF();
-                System.out.println(srvResp);
+                while (true) {
+                    srvResp = dataInputStream.readUTF();
+                    System.out.println(srvResp);
 
-                if (srvResp.startsWith("UserName")) {
-                    this.user = getChatUserName();
-                    frame.setName(this.user);
-                    out.println(this.user);
+                    if (srvResp.startsWith("UserName")) {
+                        this.user = getChatUserName();
+                        frame.setName(this.user);
+                        out.println(this.user);
+
+                    } else if (srvResp.contains("Server")) {
+                        messageArea.append(srvResp.substring(0) + "\n");
+                    }
 
                 }
-                else if (srvResp.contains("Server")) {
-                    messageArea.append(srvResp.substring(0) + "\n");
-                }
+            } catch (EOFException eof) {
+                frame.setVisible(false);
+                frame.dispose();
 
+                showByeDialog(srvResp);
+                ThreadRunFlag = false;
+
+
+                return;
+
+
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (EOFException eof) {
-            frame.setVisible(false);
-            frame.dispose();
-
-            showByeDialog(srvResp);
-
-            System.exit(0);
-
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
 
+    }
+
+    public void shutDown() {
+        this.ThreadRunFlag = false;
     }
 
     private String getChatUserName() {
@@ -105,11 +119,22 @@ public class ChatClientThread extends Thread {
     }
 
     public static void main(String[] args) {
-        Thread t1 = new ChatClientThread("client-1");
-        t1.start();
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
 
+        Thread t1 = new ChatClientThread("client-1");
         Thread t2 = new ChatClientThread("client-2");
+
+
+        t1.start();
         t2.start();
+
+        while (true) {
+            if (!t1.isAlive() & !t2.isAlive()) {
+                System.out.println("closed/.");
+                System.exit(0);
+            }
+        }
+
 
     }
 }
